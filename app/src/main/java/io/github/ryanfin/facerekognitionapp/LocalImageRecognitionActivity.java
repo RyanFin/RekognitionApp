74149c11aaf7226ce1eba7a921ab22e30bb4c41b
RyanFin <21296921@student.uwl.ac.uk>
2018-04-14 21:00:56 +0100

    Add 25% Compression Plots

--- app/src/main/java/io/github/ryanfin/facerekognitionapp/LocalImageRecognitionActivity.java
+++ app/src/main/java/io/github/ryanfin/facerekognitionapp/LocalImageRecognitionActivity.java
@@ -75,7 +75,7 @@ public class LocalImageRecognitionActivity extends Activity {
         @Override
         protected Void doInBackground(Void... in) {
 
-            Drawable myDrawable = getResources().getDrawable(R.drawable.img5);
+            Drawable myDrawable = getResources().getDrawable(R.drawable.img4);
 
             Bitmap bitmap = ((BitmapDrawable)myDrawable).getBitmap();
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
